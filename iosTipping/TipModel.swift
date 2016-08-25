b5eefffdece8cf074647b148013371a1754fccd6
Gaberdios <gaberdios@gmail.com>
2016-08-25 21:25:29 +0200

    plus model part

---
+++ iosTipping/TipModel.swift
@@ -0,0 +1,56 @@
+//
+//  TipModel.swift
+//  iosTipping
+//
+//  Created by G on 25/08/16.
+//  Copyright © 2016 gaberdios. All rights reserved.
+//
+
+import Foundation
+
+class TipCalc {
+    
+    private var _billAmount = 0.0
+    private var _tipPercent = 0.0
+    private var _tipAmount = 0.0
+    private var _totalAmount = 0.0
+    
+    var billAmount: Double {
+        get {
+            return _billAmount
+        } set {
+            _billAmount = newValue
+        }
+    }
+    
+    var tipPercent: Double {
+        get {
+            return _tipPercent
+        } set {
+            _tipPercent = newValue
+        }
+    }
+    
+    var tipAmount: Double {
+        return _tipAmount
+    }
+    
+    var totalAmount: Double {
+        return _totalAmount
+    }
+    
+    init(billAmount: Double, tipPercent: Double) {
+        self._billAmount = billAmount
+        self._tipPercent = tipPercent
+    }
+    
+    func calculateTip() {
+        _tipAmount = billAmount * tipPercent
+        _totalAmount = tipAmount +  billAmount
+    }
+    
+    
+    
+    
+}
+
